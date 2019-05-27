8a26ac5a3b95eb8e48d514715fdbb193b34bbd92
Refilwe Mekgoe <rmekgoe@gmail.com>
2019-05-27 13:58:21 +0200

    revised two files

--- blackjack.py
+++ blackjack.py
@@ -121,6 +121,10 @@ def new_game():
 def shuffle():
     random.shuffle(deck)
 
+
+def ref():
+    print("Refilwe is practicing github")
+
 mainWindow = tkinter.Tk()
 
 # Set up the screen and frames for the dealer and player
---
+++ refilwetest.py
@@ -0,0 +1,2 @@
+# This is an empty python scripts
+# with only these two comments
